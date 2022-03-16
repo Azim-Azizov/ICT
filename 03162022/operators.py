8b37895db7e8b53f7a2ded75f99978079c7e3a85
azimazizov9150 <azimazizov9150@gmail.com>
2022-03-16 17:32:51 +0400

    Create operators.py

---
+++ 03162022/operators.py
@@ -0,0 +1,29 @@
+import operator
+
+
+exe = input()
+num = ""
+nums = []
+oper = ""
+for i in exe:
+    if i in "0123456789":
+        num += i
+    elif i in "*+-/":
+        oper += f"{i} "
+        num += " "
+    else:
+        print("incorrect")
+        quit()
+for i in num.split():
+    nums += [int(i)]
+oper = oper.split()
+ops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.floordiv
+    }
+if oper[0] == "*" or oper[0] == "/":
+    print(ops[oper[1]](ops[oper[0]](nums[0], nums[1]), nums[2]))
+else:
+    print(ops[oper[0]](ops[oper[0]](nums[1], nums[2]), nums[0]))
