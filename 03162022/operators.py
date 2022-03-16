1a807e60c03e80def5c553cfcb135686b1fb1c7d
azimazizov9150 <azimazizov9150@gmail.com>
2022-03-16 17:44:04 +0400

    Update operators.py

--- 03162022/operators.py
+++ 03162022/operators.py
@@ -26,4 +26,4 @@ ops = {
 if oper[0] == "*" or oper[0] == "/":
     print(ops[oper[1]](ops[oper[0]](nums[0], nums[1]), nums[2]))
 else:
-    print(ops[oper[0]](ops[oper[0]](nums[1], nums[2]), nums[0]))
+    print(ops[oper[0]](ops[oper[1]](nums[1], nums[2]), nums[0]))
